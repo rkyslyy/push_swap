a6fa7f3ef907fde79adf089a79342cb1ac322c3a
Roman KYSLYY <rkyslyy@e1r7p3.unit.ua>
2018-02-28 17:24:56 +0200

    600 ops left

--- for_large_2/ft_fast_af_boi.c
+++ for_large_2/ft_fast_af_boi.c
@@ -13,7 +13,7 @@
 #include <stdio.h>
 #include "../swaplib.h"
 
-static void	ft_swap_rotate(t_stack **a, t_pack *pack)
+void	ft_swap_rotate(t_stack **a, t_pack *pack)
 {
 	t_stack *ptr;
 
@@ -33,6 +33,30 @@ static void	ft_swap_rotate(t_stack **a, t_pack *pack)
 			ft_reverse_rotate_a(a, pack);
 			ptr = *a;
 		}
+}
+
+void	ft_deal_with_a(t_stack **a, t_stack **b, t_pack *pack)
+{
+	t_stack *bptr;
+	t_stack *aptr;
+
+	bptr = *b;
+	aptr = *a;
+	if (aptr != NULL &&
+		ft_pick_rotate(aptr, ft_get_biggest(aptr, bptr->value)) == 1)
+		while (aptr->value != ft_get_biggest(aptr, bptr->value))
+		{
+			ft_rotate_a(a, pack);
+			aptr = *a;
+		}
+	else
+		while (aptr != NULL &&
+			aptr->value != ft_get_biggest(aptr, bptr->value))
+		{
+			ft_reverse_rotate_a(a, pack);
+			aptr = *a;
+		}
+	ft_push_from_b_to_a(a, b, pack);
 }
 
 int		ft_new_pivot(t_stack *a, int tail)
@@ -230,20 +254,29 @@ int		ft_find_size(t_stack *b, int pivot)
 	return (ret);
 }
 
+int		ft_find_next(t_stack *b, int pivot)
+{
+	t_stack *ptr;
+
+	ptr = b;
+	while (ptr->next && ptr->value >= pivot)
+		ptr = ptr->next;
+	return (ptr->value);
+}
+
 void	ft_fast_af_boi(t_stack **a, t_stack **b, t_pack *pack)
 {
 	t_stack	*aptr;
 	t_stack	*bptr;
 	int		pivot;
 	int		mem;
-	int		mina;
 	t_stack	*pivots;
 
 	aptr = *a;
 	bptr = *b;
 	pivots = NULL;
 	ft_add_node(ft_create_node(ft_get_min(*a)), &pivots);
-	while (ft_get_size(*a) > 3)
+	while (!ft_is_sorted(*a))
 	{
 		pivot = ft_get_pivot(*a, ft_get_size(*a) / 2);
 		mem = ft_get_last_value(*a);
@@ -274,70 +307,168 @@ void	ft_fast_af_boi(t_stack **a, t_stack **b, t_pack *pack)
 		}
 		ft_add_node(ft_create_node(ft_get_min(*a)), &pivots);
 	}
-//	ft_print_stacks(pivots, 0, "pivots", *pack);
-	ft_swap_rotate(a, pack);
-	ft_del_node(&pivots);
-	mina = ft_get_min(*a);
-	aptr = *a;
-	while (*a)
-		ft_push_from_a_to_b(a, b, pack);
-	while (ft_get_last_value(*b) != mina)
-		ft_rotate_b(b, pack);
-	bptr = *b;
-	aptr = *a;
-	// ft_del_node(&pivots);
-	while (pivots)
-	{
-		while (bptr->value >= pivots->value && bptr->value != ft_get_max(bptr))
+	while (pivots->value - pivots->next->value <= 10)
 		{
-			ft_push_from_b_to_a(a, b, pack);
-			aptr = *a;
-			bptr = *b;
-		}
-		while (ft_get_size(*a) > 3)
-		{
-			pivot = ft_get_pivot(*a, ft_get_size(*a) / 2);
-			mem = ft_get_last_value(*a);
-			while (aptr->value != mem)
+			ft_del_node(&pivots);
+			while (bptr->value >= pivots->value)
 			{
-				if (aptr->value <= pivot)
-				{
-					ft_push_from_a_to_b(a, b, pack);
-					aptr = *a;
-					bptr = *b;
-				}
-				else
-				{
-					ft_rotate_a(a, pack);
-					aptr = *a;
-				}
-			}
-			if (aptr->value <= pivot)
-			{
-				ft_push_from_a_to_b(a, b, pack);
+				ft_deal_with_a(a, b, pack);
 				aptr = *a;
 				bptr = *b;
 			}
-			else
-			{
-				ft_rotate_a(a, pack);
-				aptr = *a;
-			}
-			ft_add_node(ft_create_node(ft_get_min(*a)), &pivots);
 		}
-		ft_swap_rotate(a, pack);
-		ft_del_node(&pivots);
-		mina = ft_get_min(*a);
-		aptr = *a;
-		while (*a)
-			ft_push_from_a_to_b(a, b, pack);
-		while (ft_get_last_value(*b) != mina)
+	mem = ft_get_last_value(*b);
+	pivot = ft_new_pivot(*b, ft_find_next(*b, pivots->next->value));
+	printf("       %d\n", pivot);
+	ft_rotate(&pivots);
+	while (bptr->value >= pivots->value)
+	{
+		if (bptr->value >= pivot)
+			ft_push_from_b_to_a(a, b, pack);
+		else
 			ft_rotate_b(b, pack);
-		bptr = *b;
 		aptr = *a;
+		bptr = *b;
 	}
-	while (*b)
-		ft_push_from_b_to_a(a, b, pack);
+	if (ft_pick_rotate(*b, mem) == 1)
+		while (ft_get_last_value(*b) != mem)
+			ft_rotate_b(b, pack);
+	else
+		while (ft_get_last_value(*b) != mem)
+			ft_reverse_rotate_b(b, pack);
+	while (aptr->value <= ft_get_last_value(pivots))
+	{
+		ft_push_from_a_to_b(a, b, pack);
+		aptr = *a;
+		bptr = *b;
+	}
+	ft_add_node(ft_create_node(pivot), &pivots);
+	ft_reverse_rotate(&pivots);
+	while (ft_get_size(*b) >= 20)
+	{
+		while (pivots->value - pivots->next->value > 10)
+	{
+		mem = ft_get_last_value(*b);
+	pivot = ft_new_pivot(*b, ft_find_next(*b, pivots->next->value));
+	printf("       %d\n", pivot);
+	ft_rotate(&pivots);
+	while (bptr->value >= pivots->value && bptr->value != mem)
+	{
+		if (bptr->value >= pivot)
+			ft_push_from_b_to_a(a, b, pack);
+		else
+			ft_rotate_b(b, pack);
+		aptr = *a;
+		bptr = *b;
+	}
+	if (ft_pick_rotate(*b, mem) == 1)
+		while (ft_get_last_value(*b) != mem)
+			ft_rotate_b(b, pack);
+	else
+		while (ft_get_last_value(*b) != mem)
+			ft_reverse_rotate_b(b, pack);
+	while (aptr->value <= ft_get_last_value(pivots))
+	{
+		ft_push_from_a_to_b(a, b, pack);
+		aptr = *a;
+		bptr = *b;
+	}
+	ft_add_node(ft_create_node(pivot), &pivots);
+	ft_reverse_rotate(&pivots);
+	}
+	while (pivots->value - pivots->next->value <= 10)
+		{
+			ft_del_node(&pivots);
+			while (bptr->value >= pivots->value)
+			{
+				ft_deal_with_a(a, b, pack);
+				aptr = *a;
+				bptr = *b;
+			}
+		}
+	}
+	if (ft_is_sorted(*a))
+		printf("a is sorted boiiii\n");
+	else
+		printf("a is not sorted\n");
+	// if (ft_pick_rotate(aptr, ft_get_min(*a)) == 1)
+	// 	while (aptr->value != ft_get_min(*a))
+	// 	{
+	// 		ft_rotate_a(a, pack);
+	// 		aptr = *a;
+	// 	}
+	// else
+	// 	while (aptr->value != ft_get_min(*a))
+	// 	{
+	// 		ft_reverse_rotate_a(a, pack);
+	// 		aptr = *a;
+	// 	}
+	// while (ft_get_size(*a) <= 13)
+	// 	ft_deal_with_a(a, b, pack);
+//	ft_print_stacks(pivots, 0, "pivots", *pack);
+	// ft_swap_rotate(a, pack);
+	// ft_del_node(&pivots);
+	// mina = ft_get_min(*a);
+	// aptr = *a;
+	// while (*a)
+	// 	ft_push_from_a_to_b(a, b, pack);
+	// while (ft_get_last_value(*b) != mina)
+	// 	ft_rotate_b(b, pack);
+	// bptr = *b;
+	// aptr = *a;
+	// // ft_del_node(&pivots);
+	// while (pivots)
+	// {
+	// 	while (bptr->value >= pivots->value && bptr->value != ft_get_max(bptr))
+	// 	{
+	// 		ft_push_from_b_to_a(a, b, pack);
+	// 		aptr = *a;
+	// 		bptr = *b;
+	// 	}
+	// 	while (ft_get_size(*a) > 3)
+	// 	{
+	// 		pivot = ft_get_pivot(*a, ft_get_size(*a) / 2);
+	// 		mem = ft_get_last_value(*a);
+	// 		while (aptr->value != mem)
+	// 		{
+	// 			if (aptr->value <= pivot)
+	// 			{
+	// 				ft_push_from_a_to_b(a, b, pack);
+	// 				aptr = *a;
+	// 				bptr = *b;
+	// 			}
+	// 			else
+	// 			{
+	// 				ft_rotate_a(a, pack);
+	// 				aptr = *a;
+	// 			}
+	// 		}
+	// 		if (aptr->value <= pivot)
+	// 		{
+	// 			ft_push_from_a_to_b(a, b, pack);
+	// 			aptr = *a;
+	// 			bptr = *b;
+	// 		}
+	// 		else
+	// 		{
+	// 			ft_rotate_a(a, pack);
+	// 			aptr = *a;
+	// 		}
+	// 		ft_add_node(ft_create_node(ft_get_min(*a)), &pivots);
+	// 	}
+	// 	ft_swap_rotate(a, pack);
+	// 	ft_del_node(&pivots);
+	// 	mina = ft_get_min(*a);
+	// 	aptr = *a;
+	// 	while (*a)
+	// 		ft_push_from_a_to_b(a, b, pack);
+	// 	while (ft_get_last_value(*b) != mina)
+	// 		ft_rotate_b(b, pack);
+	// 	bptr = *b;
+	// 	aptr = *a;
+	// }
+	// while (*b)
+	// 	ft_push_from_b_to_a(a, b, pack);
 	// while (pivots != NULL)
 	// {
 	// 	while (bptr->value >= pivots->value)
